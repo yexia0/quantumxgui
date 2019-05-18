bf31f5085a13eb7aebb1e88fe0142fd7fd07bc1c
polaris <polaris.xia@gmail.com>
2019-05-18 21:57:25 +0800

    Support refresh

--- native/windows/test.c
+++ native/windows/test.c
@@ -6,5 +6,6 @@ void main() {
 	int r = rawhid_open(5, 0x16C0, 0x0480, 0xFFAB, 0x0200);
 	Sleep(5000);
 	rawhid_open(5, 0x16C0, 0x0480, 0xFFAB, 0x0200);
+	Sleep(30000);
 	printf("hello world: %d\n", r);
 }
